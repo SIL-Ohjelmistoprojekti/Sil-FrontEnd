59693dc15ec29c56ae465d6671e3d6c4d95f95aa
Raikku <107673429+raiccu@users.noreply.github.com>
2024-09-10 12:13:54 +0300

    placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ function App() {
     <>
       <div className='container'>
       <h1>Weather</h1>
-      <div id='temperature'>
-        <p></p>
+      <div id='current'>
+        <h2>Weather now</h2>
+        <p>Measured 10.9. 12:05</p>
+        <p id='temperature'>temperature: 21.5°</p>
+        <p id="humidity">humidity: 59%</p>
+        <p id="sun">Sun: 06:37 - 19:56</p>
       </div>
       </div>
     </>
