e6ade7e2ab38bc8ae5be7c59af638c31cb767eff
Raikku <107673429+raiccu@users.noreply.github.com>
2024-09-17 11:18:15 +0300

    current weather in own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Current from './components/Current'
 
 function App() {
 
@@ -6,13 +7,7 @@ function App() {
     <>
       <div className='container'>
       <h1>Weather</h1>
-      <div id='current'>
-        <h2>Weather now</h2>
-        <p>Measured 10.9. 12:05</p>
-        <p id='temperature'>temperature: 21.5°</p>
-        <p id="humidity">humidity: 59%</p>
-        <p id="sun">Sun: 06:37 - 19:56</p>
-      </div>
+      <Current />
       </div>
     </>
   )
