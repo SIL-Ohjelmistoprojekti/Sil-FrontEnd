534cc4949c1350a8fd16c6f1686e70483a800124
Raikku <107673429+raiccu@users.noreply.github.com>
2024-09-17 11:24:38 +0300

    Update Current.jsx

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Current = () => {
+    
   return (
-    <div><div id='current'>
-    <h2>Weather now</h2>
-    <p>Measured 10.9. 12:05</p>
-    <p id='temperature'>temperature: 21.5°</p>
-    <p id="humidity">humidity: 59%</p>
-    <p id="sun">Sun: 06:37 - 19:56</p>
-  </div></div>
-  )
-}
+    <>
+      <div id="current">
+        <h2>Weather now</h2>
+        <p>Measured 10.9. 12:05</p>
+        <p id="temperature">temperature: 21.5°</p>
+        <p id="humidity">humidity: 59%</p>
+        <p id="sun">Sun: 06:37 - 19:56</p>
+      </div>
+    </>
+  );
+};
 
-export default Current
+export default Current;
